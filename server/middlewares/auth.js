2a76f25d30140e338b336360c25967c9692bf7de
David <davidchak@yandex.ru>
2020-04-05 17:43:19 +0300

    added comment

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,14 +2,19 @@ const passport = require("passport");
 const Strategy = require("passport-http-bearer").Strategy;
 const db = require(global.appRoot + "/db");
 
-passport.use(new Strategy(
-    function (token, cb) {
-        db.findByToken(token, function (err, user) {
-            if (err) { return cb(err); }
-            if (!user) { return cb(null, false); }
-            return cb(null, user);
-        });
-    })
+// Pasport strategy
+passport.use(
+  new Strategy(function (token, cb) {
+    db.findByToken(token, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      return cb(null, user);
+    });
+  })
 );
 
 module.exports = passport;
